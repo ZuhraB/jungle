d029d5ac5a5a493818d715e7d4ec0c991948c579
ZuhraB <zuhra.bakhshi.com>
2020-10-15 18:37:36 +0000

    added basic HTTP auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  name: ENV['AUTHRIZE_USERNAME'].to_s, password: ENV['AUTHRIZE_PASSWORD'].to_s
+  http_basic_authenticate_with name: ENV['AUTHRIZE_USERNAME'].to_s, password: ENV['AUTHRIZE_PASSWORD'].to_s
   def index
     @products = Product.order(id: :desc).all
   end
